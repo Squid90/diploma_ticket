339230060682eeabfa38e662dc6f88849fbf1b76
Squid90 <squid90@rambler.ru>
2024-10-24 01:15:10 +0300

    U6

---
+++ src/components/main/Order/choose__passanger/Passanger__LeftBlock.tsx
@@ -0,0 +1,27 @@
+
+
+export const Passanger__LeftBlock: React.FC = () => {
+  return (
+    <>
+        <div className='passangers__leftBlock__detailsTrip'>
+            Детали поездки
+        </div>
+        <div className='passangers__leftBlock__line'></div>
+        <div className='passangers__leftBlock__direction-there'>
+            Туда
+        </div>
+        <div className='passangers__leftBlock__line'></div>
+        <div className='passangers__leftBlock__direction-back'>
+            Обратно
+        </div>
+        <div className='passangers__leftBlock__line'></div>
+        <div className='passangers__leftBlock__passangers'>
+            Пассажиры
+        </div>
+        <div className='passangers__leftBlock__line'></div>
+        <div className='passangers__leftBlock__totalCost'>
+            Итого
+        </div>
+    </>
+  )
+}
--- src/components/main/Order/choose__train/Choose__train.tsx
+++ src/components/main/Order/choose__train/Choose__train.tsx
@@ -6,6 +6,7 @@ import { Choose__train__mainSide } from './choose__train__mainSide';
 import { Choose__place__mainSide } from '../choose__place/choose__place__mainSide';
 import { Routes, Route } from 'react-router-dom';
 import { Choose__passangers__mainSide } from '../choose__passanger/choose__passangers__mainSide';
+import { Passanger__LeftBlock } from '../choose__passanger/Passanger__LeftBlock';
 
 export const Choose__train: React.FC = () => {
   
@@ -13,10 +14,18 @@ export const Choose__train: React.FC = () => {
     <div className='choose__train'>
       <div className='choose__train__leftSide'>
         <div className='choose__filter'>
-          <Choose__filter />
+        <Routes>
+            <Route path="/" element={<Choose__filter />} />
+            <Route path="/chooseplace" element={<Choose__filter />} />
+            <Route path="/chooseplace/choosepassangers" element={<Passanger__LeftBlock />} />
+        </Routes>
         </div>
         <div className='choose__lastTicket'>
-          <Choose__lastTicket />
+          <Routes>
+            <Route path="/" element={<Choose__lastTicket />} />
+            <Route path="/chooseplace" element={<Choose__lastTicket />} />
+            <Route path="/chooseplace/choosepassangers/*" element={''} />
+        </Routes>
         </div>
       </div>
       <div className='choose__train__mainSide' id='chooseTrainMainSide'>
