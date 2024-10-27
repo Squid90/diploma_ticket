5a675d3259ff05973721d419414c8bea15090b54
Squid90 <squid90@rambler.ru>
2024-10-27 17:04:05 +0300

    U8

--- src/components/main/Order/choose_pay/Choose__pay__mainSide.tsx
+++ src/components/main/Order/choose_pay/Choose__pay__mainSide.tsx
@@ -1,12 +1,132 @@
+import { Field, Form, Formik } from 'formik';
 import './Choose__pay.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Choose__pay__mainSide: React.FC = () => {
-
+    document.querySelector('.bar2')?.classList.add('active__bar');
     document.querySelector('.bar3')?.classList.add('active__bar');
 
+    const [isCheckedOnline, setIsCheckedOnline] = useState(false);
+    const [isCheckedCash, setIsCheckedCash] = useState(false);
+    const changeColorTextOnline = () => {
+      setIsCheckedOnline(prevState => !prevState);
+    }
+    const changeColorTextCash = () => {
+      setIsCheckedCash(prevState => !prevState);
+    }
 
   return (
-    <div className='choose_pay_mainSide'>Choose__pay</div>
+    <div className='choose_pay_mainSide'>
+      <div className='choose_pay_mainSide_formData'>
+        <Formik>
+          <Form>
+            <div className='choose_pay_mainSide_title'>
+              Персональные данные
+            </div>
+            <div className='choose_pay_mainSide_line'></div>
+
+            <div className='choose_pay_mainSide_personalData'>
+              <div className='choose_pay_mainSide_personalData_fio'>
+                <div className='choose_pay_mainSide_personalData_label label_surname'>
+                  Фамилия
+                </div>
+                <div className='choose_pay_mainSide_personalData_label label_name'>
+                  Имя
+                </div>
+                <div className='choose_pay_mainSide_personalData_label label_middlename'>
+                  Отчество
+                </div>
+                <Field
+                  className='choose_pay_mainSide_personalData_input input_surname'
+                  value='Фамилия'
+                  name='surname'
+                  type='text' />
+                <Field
+                  className='choose_pay_mainSide_personalData_input input_name'
+                  value='Имя'
+                  name='name'
+                  type='text' />
+                <Field
+                  className='choose_pay_mainSide_personalData_input input_middlename'
+                  value='Отчество'
+                  name='middlename'
+                  type='text' />
+              </div>
+              <div className='choose_pay_mainSide_personalData_phoneemailBlock phone_Block'>
+                <div className='choose_pay_mainSide_personalData_label label_phone'>
+                  Контактный телефон
+                </div>
+                <Field
+                  className='choose_pay_mainSide_personalData_input input_phone'
+                  placeholder='+7 ___ ___ __ __'
+                  name='phone'
+                  type='text' />
+              </div>
+              <div className='choose_pay_mainSide_personalData_phoneemailBlock email_Block'>
+                <div className='choose_pay_mainSide_personalData_label label_email'>
+                  E-mail
+                </div>
+                <Field
+                  className='choose_pay_mainSide_personalData_input input_email'
+                  placeholder='inbox@example.ru'
+                  name='email'
+                  type='text' />
+              </div>
+            </div>
+            <div className='choose_pay_mainSide_line'></div>
+
+            <div className='choose_pay_mainSide_title'>
+              Способ оплаты
+            </div>
+            <div className='choose_pay_mainSide_line'></div>
+
+            <div className='choose_pay_mainSide_onlinePay'>
+              <div className='choose_pay_mainSide_checkboxBlock'>
+                <label className='choose_pay_mainSide_labelCheckbox'>
+                  <input
+                    className='choose_pay_mainSide_checkbox'
+                    type='checkbox'
+                    onChange={changeColorTextOnline} />
+                  <span className="choose_pay_mainSide_checkboxMask"></span>
+                </label>
+                <div
+                  className='choose_pay_mainSide_checkboxText'
+                  style={{ color: isCheckedOnline ? '#FFA800' : '#928F94' }}>Онлайн</div>
+              </div>
+              <div className='choose_pay_mainSide_onlinePay_methods'>
+                <div className='choose_pay_mainSide_onlinePay_method'>
+                  Банковской картой
+                </div>
+                <div className='choose_pay_mainSide_onlinePay_method'>
+                  PayPal
+                </div>
+                <div className='choose_pay_mainSide_onlinePay_method'>
+                  Visa QIWI Wallet
+                </div>
+              </div>
+            </div>
+            <div className='choose_pay_mainSide_line'></div>
+
+            <div className='choose_pay_mainSide_cashPay'>
+              <div className='choose_pay_mainSide_checkboxBlock'>
+                <label className='choose_pay_mainSide_labelCheckbox'>
+                  <input
+                    className='choose_pay_mainSide_checkbox'
+                    type='checkbox'
+                    onChange={changeColorTextCash} />
+                  <span className="choose_pay_mainSide_checkboxMask"></span>
+                </label>
+                <div
+                  className='choose_pay_mainSide_checkboxText'
+                  style={{ color: isCheckedCash ? '#FFA800' : '#928F94' }}>Наличными</div>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+      <div className='choose_pay_mainSide_nextPage'>
+            <button className='choose_pay_mainSide_nextPagebtn'>Купить билеты</button>
+      </div>
+    </div>
   )
 }
