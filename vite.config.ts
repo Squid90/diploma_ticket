b020a191a584d43ae276e545c9bd25a8c548d5ce
Squid90 <squid90@rambler.ru>
2024-10-31 22:18:52 +0300

    U14

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  base: '/diploma_ticket',
-  
+  base: '/diploma_ticket/',
+
   plugins: [
     react(),
   ],
