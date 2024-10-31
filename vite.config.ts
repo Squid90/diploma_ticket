b93c4cefaa2eda421fb8ab921e986447e4c5adea
Squid90 <squid90@rambler.ru>
2024-10-31 21:42:13 +0300

    U14

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,14 @@
-import path from 'path';
-import { VitePluginRadar } from 'vite-plugin-radar'
-
 import { defineConfig } from 'vite';
-import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react';
-//import 'dotenv/config';
-
-const metrica = ((id?: string) => {
-  if (!id) {
-    return;
-  }
-  return [
-    {
-      id,
-      config: {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-      }
-    }
-  ]
-})(process.env.APP_METRIKA_ID);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  plugins: [
-    svgr(), 
-    react(),
-    VitePluginRadar({
-      enableDev: false,
-      metrica
-    })
-  ],
+  
   base: '/diploma_ticket',
-  build: {
-    outDir: 'dist'
-  },
-  preview: {
-    port: 8080
-  },
-  assetsInclude: [
-    '**/*.ttf'
-  ]
+  
+  plugins: [
+    react(),
+  ],
+  
+
 })
